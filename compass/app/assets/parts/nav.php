ed66a267c3108d15a180d9c4fad22be7e9c12991
Nico <lahondes.n@gmail.com>
2022-01-19 17:22:37 +0100

    v1 inital commit correction 2 added app folder

---
+++ compass/app/assets/parts/nav.php
@@ -0,0 +1,12 @@
+    <!-- Contains navbar -->
+    <nav>
+      <div class="container">
+        <div>
+          <a href="#">Home</a>
+          <a href="#" id="discover">Discover</a>
+        </div>
+        <div>
+          <a href="#" id="signin">Sign in</a>
+        </div>
+      </div>
+    </nav>
---
+++ compass/app/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/public/css/style.min.css">
+    <title>Document</title>
+</head>
+<body>
+  <?php require_once 'assets/parts/nav.php'?>
+  <section class="one">
+  </section>
+
+  <section class="two">
+  </section>
+
+  <section class="three"></section>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/ScrollTrigger.min.js"></script>
+  <script src="js/nav.js"></script>
+</body>
+</html>
+
