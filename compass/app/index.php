02b0c800bc1333f1ac56a030d9e6c58bba97b204
Nico <lahondes.n@gmail.com>
2022-03-10 16:12:42 +0100

    Front page album and artist

--- compass/app/index.php
+++ compass/app/index.php
@@ -5,6 +5,5 @@ require 'vendor/autoload.php';
 
 // Launch Router with the $_SERVER['REQUEST_URI'] as parameter. 
 $router = new Router($_SERVER['REQUEST_URI']);
-
 // Search if requested url exist
 $router->run();
